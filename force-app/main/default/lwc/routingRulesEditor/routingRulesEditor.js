aea0fa71307a54c794043d3bfb84897eb4c984da
Tanner Welton <twelton@qualtrics.com>
2021-04-22 02:31:11 -0600

    most of UI

---
+++ force-app/main/default/lwc/routingRulesEditor/routingRulesEditor.js
@@ -0,0 +1,152 @@
+import { LightningElement, api } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+
+import codeflask from '@salesforce/resourceUrl/codeflask'
+
+const functions = [
+    // logical
+    {
+        name: "AND",
+        group: "Logical",
+        value: "AND(#,#)"
+    },
+    {
+        name: "OR",
+        group: "Logical",
+        value: "OR(#,#)"
+    },
+    {
+        name: "ISBLANK",
+        group: "Logical",
+        value: "ISBLANK(#)"
+    },
+    {
+        name: "NOT",
+        group: "Logical",
+        value: "NOT(#)"
+    },
+    // text manipulations
+    {
+        name: "BEGINS",
+        group: "Text Manipulation",
+        value: "BEGINS(#,#)"
+    },
+    {
+        name: "ENDS",
+        group: "Text Manipulation",
+        value: "ENDS(#,#)"
+    },
+    {
+        name: "CONTAINS",
+        group: "Text Manipulation",
+        value: "BEGINS(#,#)"
+    },
+    {
+        name: "LEFT",
+        group: "Text Manipulation",
+        value: "LEFT(#,#)"
+    },
+    {
+        name: "RIGHT",
+        group: "Text Manipulation",
+        value: "RIGHT(#,#)"
+    },
+    // value conversion
+    {
+        name: "TEXT",
+        group: "Value Conversion",
+        value: "TEXT(#)"
+    },
+    {
+        name: "VALUE",
+        group: "Value Conversion",
+        value: "VALUE(#)"
+    },
+    // miscellaneous
+    {
+        name: "LEN",
+        group: "Miscellaneous",
+        value: "LEN(#)"
+    }
+]
+
+export default class RoutingRulesEditor extends LightningElement {
+
+    @api rule
+    cursorPosition = null
+    editor
+
+    // Getters
+    get groupedFunctions() {
+        let groupings = functions.reduce((objectsByKeyValue, obj) => {
+            const value = obj.group;
+            objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
+            return objectsByKeyValue;
+        }, {});
+
+        return Object.entries(groupings).map(grouping => ({ name: grouping[0], functions: grouping[1]}))
+    }
+
+    handleInsertFunctionClick(event) {
+        const functionString = functions.find(f => f.name === event.target.value)?.value
+        
+        if(functionString) {
+            this.insertAtCursorHead(functionString)
+        }
+    }
+
+    handleInsertFieldClick(event) {
+        console.log(event.target.value)
+        console.log(this.cursorPosition)
+    }
+
+    handleSaveClick() {
+        let updatedRule = JSON.parse(JSON.stringify(this.rule))
+        updatedRule.Formula__c = this.editor.getCode()
+        const saveEvent = new CustomEvent('save', {
+            detail: {
+              rule: updatedRule
+            }
+        })
+        // Fire the custom event
+        this.dispatchEvent(saveEvent)
+        this.handleCloseClick()
+    }
+
+    handleCloseClick() {
+        const closeEvent = new CustomEvent('close')
+        // Fire the custom event
+        this.dispatchEvent(closeEvent)
+    }
+
+    async insertAtCursorHead(string) {
+        let cursorPosition = this.template.querySelector('.editor textarea').selectionStart
+
+        // retrieve current editor content
+        const oldEditorContent = this.editor.getCode()
+
+        // add new text at cursor head
+        let newEditorContent = oldEditorContent.slice(0,cursorPosition) + string + oldEditorContent.slice(cursorPosition)
+
+        // update entire editor content
+        await this.editor.updateCode(newEditorContent)
+
+        // advance cursor position to end of new text
+        cursorPosition += string.length
+
+        // refocus editor and set cursor
+        let editorElement = this.template.querySelector('.editor textarea')
+        editorElement.focus();
+        editorElement.setSelectionRange(cursorPosition, cursorPosition)
+    }
+
+    // Lifecycle Methods
+
+    async connectedCallback() {
+        await loadScript(this, codeflask)
+        console.log(this.groupedFunctions)
+        this.editor = new CodeFlask(this.template.querySelector('.editor'), { language: 'js', lineNumbers: true });
+        this.editor.updateCode(this.rule?.Formula__c)
+    }
+
+}
---
+++ force-app/main/default/lwc/routingRulesManager/routingRulesManager.js
@@ -0,0 +1,115 @@
+import { LightningElement } from 'lwc';
+
+import savePricing from '@salesforce/apex/RSPricingDealApproval.savePricing';
+
+export default class RoutingRulesManager extends LightningElement {
+
+    currentTab = "rules/Case"
+    currentTier = null
+    rules
+    selectedRule = null
+    showEditor = false
+
+    get filteredRules() {
+        console.log('tier: ', this.currentTier)
+        console.log('rules: ', this.rules)
+        console.log(this.rules.filter(rule => rule.Object_Developer_Name__c === this.currentTab.replace('rules/','') && rule.Parent__c === this.currentTier?.Id))
+        return this.rules.filter(rule => rule.Object_Developer_Name__c === this.currentTab.replace('rules/','') && rule.Parent__c === this.currentTier?.Id)
+    }
+
+    get actionTypeOptions() {
+        return [
+            { label: 'Field Assignment', value: 'Field Assignment' },
+            { label: 'Evaluate Child Rules', value: 'Evaluate Child Rules' },
+        ];
+    }
+
+    get parentRuleChain() {
+        if(this.currentTier) {
+            let currentRuleLevel = this.currentTier
+            let ruleChain = []
+            while (currentRuleLevel) {
+                // add to front list
+                ruleChain.unshift(currentRuleLevel)
+
+                // set current level to parent for next loop iteration
+                // find parent rule in list of all rules
+                currentRuleLevel = this.rules.find(rule => rule.Id === currentRuleLevel.Parent__c)                
+            }
+            return ruleChain
+        }
+        return null
+    }
+
+    get evaluatesChildren() {
+        return this.selectedRule.Action_Type__c === 'Evaluate Child Rules'
+    }
+
+    toggleEditor() {
+        this.showEditor = !this.showEditor
+    }
+
+    saveFormula(event) {
+        this.selectedRule = event?.detail?.rule
+    }
+
+    updateActionType(event) {
+        this.selectedRule.Action_Type__c = event.detail.value
+    }
+    
+    handleSelect(event) {
+        this.currentTab = event.detail.name
+    }
+
+    handleSelectRule(event) {
+        console.log(event.target.getAttribute('data-rule'))
+        this.selectedRule = this.rules.find(rule => rule.Id === event.target.getAttribute('data-rule'))
+        console.log(this.selectedRule)
+    }
+
+    handleShowChildrenClick() {
+        this.currentTier = this.selectedRule
+        this.selectedRule = null
+        console.log(this.currentTier)
+    }
+
+    handleBackClick() {
+        this.currentTier = this.rules.find(rule => rule.Id === this.currentTier.Parent__c)
+    }
+
+    connectedCallback() {
+        this.rules = [
+            {
+                sobjectType: 'Routing_Rule__c',
+                Id: '0',
+                Name: 'All Finance Cases',
+                Formula__c: '{RecordType.Name} == "Finance"',
+                Action_Type__c: 'Field Assignment',
+                Action_Field_Developer_Name__c: 'Owner',
+                Action_Field_Value__c: '0052J000008I9DMQA0',
+                Object_Developer_Name__c: 'Case'
+            },
+            {
+                sobjectType: 'Routing_Rule__c',
+                Id: '1',
+                Name: 'All SFDC Support Cases',
+                Formula__c: '{RecordType.Name} == "CPQ Admin Support"',
+                Action_Type__c: 'Evaluate Child Rules',
+                Action_Field_Developer_Name__c: '',
+                Action_Field_Value__c: '',
+                Object_Developer_Name__c: 'Case'
+            },
+            {
+                sobjectType: 'Routing_Rule__c',
+                Id: '2',
+                Name: 'All Other Cases',
+                Parent__c: '1',
+                Formula__c: '{RecordType.Name} == "CPQ Admin Support"',
+                Action_Type__c: 'Evaluate Child Rules',
+                Action_Field_Developer_Name__c: '',
+                Action_Field_Value__c: '',
+                Object_Developer_Name__c: 'Case'
+            }
+        ]
+    }
+}
